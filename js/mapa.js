36ca10793114e83de307bd559bdecc2b5b50b20f
mavaloso <60003102+mavaloso@users.noreply.github.com>
2023-04-10 19:31:16 -0500

    vewr2
    Extencion de archivos

---
+++ js/mapa.js
@@ -0,0 +1,12 @@
+let map = L.map('map').setView([-6.77137,-79.84088],6)
+
+//Agregar tilelAyer mapa base desde openstreetmap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+document.getElementById('select-location').addEventListener('change',function(e){
+  let coords = e.target.value.split(",");
+  map.flyTo(coords,9);
+})
+
